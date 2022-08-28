13d6c7b7a8d2eaaf0f0bbd592c82147c5e2441c5
Piotr Kaczmarski <piotr.kaczmarski@onet.pl>
2022-08-29 01:40:35 +0200

    Issue #1 - WiP

--- Assets/Editor/MyWindow.cs
+++ Assets/Editor/MyWindow.cs
@@ -7,27 +7,104 @@ using UnityEditor;
 public class WizardCreatePackage : ScriptableWizard
 {
 
+    public string companyName;
     public string packageName;
-    public string resultName;
     
     [MenuItem("MotherPacker/Create Package")]
     static void CreateWizard()
     {
-        ScriptableWizard.DisplayWizard<WizardCreatePackage>("Create Package", "Create", "Apply");
+        ScriptableWizard.DisplayWizard<WizardCreatePackage>("Create Package", "Create");
     }
 
     void OnWizardCreate()
     {
+      
         var dataPath = Application.dataPath.Substring(0, Application.dataPath.Length - 6);
-        Directory.CreateDirectory(dataPath + "Packages" + Path.DirectorySeparatorChar + packageName);
+
+        var packagesPath = dataPath + "Packages";
+
+        var packageDirName = "com." + this.companyName.ToLower() + "." + this.packageName.ToLower();
         
+        string packagePath = packagesPath + Path.DirectorySeparatorChar + packageDirName;
+        
+        Directory.CreateDirectory(packagePath);
+
+        var writer = File.CreateText(packagePath + Path.DirectorySeparatorChar + "package.json");
+        writer.Write(PACKAGE_JSON, this.companyName.ToLower(), this.packageName.ToLower());
+        writer.Flush();
+        writer.Close();
+        
+        Directory.CreateDirectory(packagePath + Path.DirectorySeparatorChar + "Runtime");
+        Directory.CreateDirectory(packagePath + Path.DirectorySeparatorChar + "Runtime" + Path.DirectorySeparatorChar + "Scripts");
+        
+        writer = File.CreateText(packagePath + Path.DirectorySeparatorChar + "Runtime" + Path.DirectorySeparatorChar + "Test.asmdef");
+        writer.Write(ASMDEF, this.companyName.ToLower(), this.packageName.ToLower());
+        writer.Flush();
+        writer.Close();
+
+        var gitIgnoreFileName = packagesPath + Path.DirectorySeparatorChar + ".gitignore";
+        if (!File.Exists(gitIgnoreFileName))
+        {
+            File.Create(gitIgnoreFileName);
+        }
+
+        using (FileStream gitIgnoreStream = new FileStream(gitIgnoreFileName, FileMode.Append, FileAccess.Write))
+        using (var gitIgnoreWriter = new StreamWriter(gitIgnoreStream))
+        {
+            gitIgnoreWriter.WriteLine(packageDirName);
+            gitIgnoreWriter.Flush();
+        }
     }
 
     void OnWizardUpdate()
     {
-        helpString = "Please set the color of the light!";
-        resultName = Application.dataPath.Substring(0, Application.dataPath.Length - 6);
-        resultName += "Packages" + Path.DirectorySeparatorChar + packageName;
+        isValid = false;
+        if (companyName == null || companyName.Trim().Equals(""))
+        {
+            errorString = "Please fill in company name";
+            return;
+        }
+        if (packageName == null || packageName.Trim().Equals(""))
+        {
+            errorString = "Please fill in package name";
+            return;
+        }
+
+        errorString = null;
+        isValid = true;
     }
-    
+
+
+    private static string PACKAGE_JSON = @"{{
+  ""name"": ""com.{0}.{1}"",
+  ""version"": ""0.0.2"",
+  ""displayName"": ""{1}"",
+  ""description"": ""This is an example package"",
+  ""unity"": ""2020.3"",
+  ""unityRelease"": ""32f1"",
+  ""documentationUrl"": ""https://example.com/"",
+  ""changelogUrl"": ""https://example.com/changelog.html"",
+  ""licensesUrl"": ""https://example.com/licensing.html"",
+  ""author"": {{
+    ""name"": ""Piotr Kaczmarski"",
+    ""email"": ""peter@pixelheartsoftware.com"",
+    ""url"": ""https://pixelheartsoftware.com""
+  }},
+  ""publishConfig"": {{
+	""registry"": ""http://localhost:4873""
+  }}
+}}";
+
+    private static string ASMDEF = @"{{
+  ""name"": ""{0}.{1}"",
+  ""includePlatforms"": [],
+  ""excludePlatforms"": [],
+  ""allowUnsafeCode"": false,
+  ""overrideReferences"": false,
+  ""precompiledReferences"": [],
+  ""autoReferenced"": true,
+  ""defineConstraints"": [],
+  ""noEngineReferences"": false
+}}";
+
 }
